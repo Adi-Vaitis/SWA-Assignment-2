201e5f7ceb7e723012866fddab59dc01e81156ed
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-09-28 23:38:07 +0200

    done other tests

--- functional/src/board.ts
+++ functional/src/board.ts
@@ -3,6 +3,7 @@ import {
   checkVerticalMatch,
   constructEffectsForPosition,
   constructIndexOfDesiredPosition,
+  effectsAreEqual,
   matchesExist,
   positionExistsInBoard,
   positionsAreInTheSameColumnOrRow,
@@ -32,7 +33,6 @@ export type BoardEvent<T> = {
   kind: "Match" | "Refill";
   match?: Match<T>;
 };
-
 
 export type Effect<T> = {
   kind: "Match" | "Refill";
@@ -131,88 +131,150 @@ export function move<T>(
   };
 
   const effects: Effect<T>[] = [];
-  const matchExists = matchesExist(newBoard, first, second);
-  if (matchExists) {
-    let effectsForFirstPosition = constructEffectsForPosition(newBoard, first);
-    let effectsForSecondPosition = constructEffectsForPosition(newBoard, second);
-    
-    effects.push(...effectsForFirstPosition, ...effectsForSecondPosition);
-  }
-  if (effects.some((effect) => effect.kind === "Match")) {
-    effects.push({ kind: "Refill", board: refillBoard(generator, newBoard) });
-  }
+  let firstPosition = first;
+  let secondPosition = second;
+  let matchesExists = matchesExist(newBoard, firstPosition, secondPosition);
+  while (matchesExists) {
+    let newEffects : Effect<T>[] = [
+      ...constructEffectsForPosition(newBoard, firstPosition),
+      ...constructEffectsForPosition(newBoard, secondPosition)
+    ];
+    effects.push(...newEffects);
 
+    let positionsMatched: Position[] = [
+      ...newEffects.filter((effect) => effect.board !== undefined || effect.match !== undefined).map((effect) => effect.match.positions).flat()
+    ]
+    // if(effectsForFirstPosition[0].match !== undefined){
+    //   positionsMatched.push(...effectsForFirstPosition[0].match.positions);
+    // }
+    //   ...effectsForSecondPosition[0].match.positions,
+    // ];
+    newBoard = {
+      ...newBoard,
+      board: refillBoard(generator, newBoard, positionsMatched).board,
+    };
+    effects.push({ kind: "Refill" });
+    /// TODO implement cascading matches
+    matchesExists = false;
+    // here we will need to set the first and second position again for cascading effect
+    // firstPosition = first;
+    // secondPosition = second;
+  }
   return { board: newBoard, effects };
 }
 
-function handleCascadingMatches<T>(
+// function handleCascadingMatches<T>(
+//   generator: Generator<T>,
+//   board: Board<T>
+// ): Effect<T>[] {
+//   const cascadeEffects: Effect<T>[] = [];
+//   let cascadeOccurred = false;
+
+//   for (let row = 0; row < board.height; row++) {
+//     for (let col = 0; col < board.width; col++) {
+//       const currentPiece = piece(board, { row, col });
+//       if (currentPiece === undefined) continue;
+
+//       const horizontalMatch = matchesExist(
+//         board,
+//         { row, col },
+//         { row, col: col + 1 }
+//       );
+//       const verticalMatch = matchesExist(
+//         board,
+//         { row, col },
+//         { row: row + 1, col }
+//       );
+
+//       if (horizontalMatch || verticalMatch) {
+//         board.board.flat()[row * board.width + col] = undefined;
+
+//         const positions: Position[] = [{ row, col }];
+//         const matchedPiece = currentPiece;
+//         if (horizontalMatch) {
+//           positions.push({ row, col: col + 1 });
+//         }
+//         if (verticalMatch) {
+//           positions.push({ row: row + 1, col });
+//         }
+//         cascadeEffects.push({
+//           kind: "Match",
+//           match: { matched: matchedPiece, positions },
+//         });
+
+//         cascadeOccurred = true;
+//       }
+//     }
+//   }
+
+//   if (cascadeOccurred) {
+//     cascadeEffects.push({
+//       kind: "Refill",
+//       board: refillBoard(generator, board),
+//     });
+//   }
+
+//   return cascadeEffects;
+// }
+
+function refillBoard<T>(
   generator: Generator<T>,
-  board: Board<T>
-): Effect<T>[] {
-  const cascadeEffects: Effect<T>[] = [];
-  let cascadeOccurred = false;
+  board: Board<T>,
+  matchedPositions: Position[]
+): Board<T> {
+  const newBoard: T[][] = [];
 
   for (let row = 0; row < board.height; row++) {
+    const newRow: T[] = [];
     for (let col = 0; col < board.width; col++) {
-      const currentPiece = piece(board, { row, col });
-      if (currentPiece === undefined) continue;
+      const currentPosition: Position = { row, col };
+      const currentPiece = piece(board, currentPosition);
 
-      const horizontalMatch = matchesExist(
-        board,
-        { row, col },
-        { row, col: col + 1 }
-      );
-      const verticalMatch = matchesExist(
-        board,
-        { row, col },
-        { row: row + 1, col }
-      );
-
-      if (horizontalMatch || verticalMatch) {
-        board.board.flat()[row * board.width + col] = undefined;
-
-        const positions: Position[] = [{ row, col }];
-        const matchedPiece = currentPiece;
-        if (horizontalMatch) {
-          positions.push({ row, col: col + 1 });
-        }
-        if (verticalMatch) {
-          positions.push({ row: row + 1, col });
-        }
-        cascadeEffects.push({
-          kind: "Match",
-          match: { matched: matchedPiece, positions },
-        });
-
-        cascadeOccurred = true;
-      }
-    }
-  }
-
-  if (cascadeOccurred) {
-    cascadeEffects.push({
-      kind: "Refill",
-      board: refillBoard(generator, board),
-    });
-  }
-
-  return cascadeEffects;
-}
-
-function refillBoard<T>(generator: Generator<T>, board: Board<T>): Board<T> {
-  const newBoard: T[][] = [];
-  for (let col = 0; col < board.width; col++) {
-    const newColumn: T[] = [];
-    for (let row = board.height - 1; row >= 0; row--) {
-      const currentPiece = piece(board, { row, col });
-      if (currentPiece === undefined) {
-        newColumn.unshift(generator.next());
+      if (
+        matchedPositions.some((matchedPos) =>
+          arePositionsEqual(matchedPos, currentPosition)
+        )
+      ) {
+        // If the current position matches one of the matched positions, replace it with a new piece.
+        newRow.push(generator.next());
       } else {
-        newColumn.unshift(currentPiece);
+        // Otherwise, keep the current piece.
+        newRow.push(currentPiece);
       }
     }
-    newBoard.push(newColumn);
+    newBoard.push(newRow);
   }
 
   return { ...board, board: newBoard };
+}
+
+// this shifts the pieces down but the first test won't work, so we need to do it differently for both cases
+// function refillBoard<T>(
+//   generator: Generator<T>,
+//   board: Board<T>,
+//   matchedPositions: Position[]
+// ): Board<T> {
+//   const newBoard: T[][] = [];
+
+//   // Create a new board with the same dimensions, leaving the top row empty.
+//   for (let row = 0; row < board.height; row++) {
+//     const newRow: T[] = [];
+//     for (let col = 0; col < board.width; col++) {
+//       if (row === 0) {
+//         // Fill the top row with new pieces.
+//         newRow.push(generator.next());
+//       } else {
+//         // Shift every other row down.
+//         newRow.push(piece(board, { row: row - 1, col }));
+//       }
+//     }
+//     newBoard.push(newRow);
+//   }
+
+//   return { ...board, board: newBoard };
+// }
+
+// Helper function to check if two positions are equal.
+function arePositionsEqual(pos1: Position, pos2: Position): boolean {
+  return pos1.row === pos2.row && pos1.col === pos2.col;
 }
